375d7b99a1a62603e57d311fd6eca56dd7c38990
Hendrick <retshephilengm@gmail.com>
2024-09-19 19:21:23 +0200

    update

--- Agency/src/App.tsx
+++ Agency/src/App.tsx
@@ -1,8 +1,27 @@
 import TopPage from './TopPage';
 
 function App(){
+
+    const time = new Date()
+    const hrs = time.getHours();
+    const min = time.getMinutes();
+
  return(
-  <TopPage></TopPage>
+ <>
+  <header>
+            <div>
+                <span><img src='assets/Logo-1.png'/></span>
+                <text>Evo Creatives</text>
+            </div>
+            <div><p>Calgary, Canada {hrs + ' : ' + min} PM</p></div>
+            <div>
+                <text>Menu</text>
+                <span><img src=''/></span>
+            </div>
+        </header>
+        <TopPage></TopPage>
+ </>
+ 
  )
 }
 
