9ebf8520c7c7bc1a50c554f3979e7f7fe51af7c3
Hendrick <retshephilengm@gmail.com>
2024-09-20 10:41:45 +0200

    updated

--- Agency/src/App.tsx
+++ Agency/src/App.tsx
@@ -1,30 +1,32 @@
 import HeroSection from './page2/HeroSection';
 import TopPage from './TopPage';
+import logo from './assets/Icon mark 1.png'
+import menu_icon from './assets/menu-icon.png'
+import './App.css'
 
-function App(){
+function App() {
 
     const time = new Date()
     const hrs = time.getHours();
     const min = time.getMinutes();
 
- return(
- <>
-  <header>
-            <div>
-                <span><img src='assets/Logo-1.png'/></span>
-                <text>Evo Creatives</text>
-            </div>
-            <div><p>Calgary, Canada {hrs + ' : ' + min} PM</p></div>
-            <div>
-                <text>Menu</text>
-                <span><img src=''/></span>
-            </div>
-        </header>
-        <TopPage></TopPage>
-
-  <HeroSection/>
- </>
- )
+    return (
+        <>
+            <header>
+                <div id='lgcn'>
+                    <span><img src={logo}  id='logo'/></span>
+                    <text>Evo Creatives</text>
+                </div>
+                <div><p>Calgary, Canada {hrs + ' : ' + min} PM</p></div>
+                <div id='menu'>
+                    <text>Menu</text>
+                    <span><img src={menu_icon} id='menu_icon'/></span>
+                </div>
+            </header>
+            <TopPage></TopPage>
+            <HeroSection />
+        </>
+    )
 }
 
 export default App;
